68e2bab42ef70c0d546073a1c23d272503c2c18b
章文炳 <zhangwenbing@cic.cn>
2024-06-14 12:04:42 +0800

    add sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'great_sre'
+import sphinx_rtd_theme
+
+project = 'cic-sre'
 copyright = '2024, textworld'
 author = 'textworld'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['recommonmark','sphinx_markdown_tables'] 
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-language = 'zh'
+language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,10 +1,10 @@
-.. great_sre documentation master file, created by
-   sphinx-quickstart on Fri Jun 14 11:09:55 2024.
+.. cic-sre documentation master file, created by
+   sphinx-quickstart on Fri Jun 14 11:57:36 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to great_sre's documentation!
-=====================================
+Welcome to cic-sre's documentation!
+===================================
 
 .. toctree::
    :maxdepth: 2
